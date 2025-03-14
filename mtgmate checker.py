c1bb5b5b422522626ace249e57b51c3b2433ed07
brhyeton <brhyeton.hall@gmail.com>
2025-03-14 17:20:13 +1100

    initial upload

---
+++ mtgmate checker.py
@@ -0,0 +1,286 @@
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PyQt6.QtCore import QThread, pyqtSignal, QMutex, QWaitCondition
+from PyQt6 import uic
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep, time
+import csv
+import requests
+import numpy as np
+import configparser
+import os
+
+class WorkerThread(QThread):
+    progress_update = pyqtSignal(int)
+    show_full_message = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.mutex = QMutex()
+        self.wait_condition = QWaitCondition()
+
+    def details(self, username, password, collection_path, spare_quantity, logBox, itemsPerSec):
+        
+        self.username = username
+        self.password = password
+        self.collection_path = collection_path
+        self.spare_quantity = int(spare_quantity)
+        self.logBox = logBox
+        self.itemsPerSec = itemsPerSec
+        self.collection = []
+        self.cards = []
+        self.log = []
+        
+    def update_log_box(self, text):
+        self.log.append(text)
+        full_text = ''
+        for entry in self.log:
+            full_text += '\n' + entry
+            
+        self.logBox.setText(full_text)
+        
+    def resume(self):
+        self.mutex.lock()
+        self.wait_condition.wakeAll()
+        self.mutex.unlock()
+        
+    def run(self):
+        driver = webdriver.Chrome()
+        driver.get("https://www.mtgmate.com.au/cards/buylist_search")
+        self.update_log_box("Site opened.")
+        sleep(2)
+
+        #enter login details
+        username_box = driver.find_element(
+            By.XPATH, '/html/body/div[1]/form/div[1]/div[1]/input')
+        username_box.send_keys(self.username)
+        password_box = driver.find_element(
+            By.XPATH, '/html/body/div[1]/form/div[1]/div[2]/input')
+        password_box.send_keys(self.password)
+        login_button = driver.find_element(
+            By.XPATH, '/html/body/div[1]/form/div[2]/input')
+        login_button.click()
+        
+        search_box = driver.find_element(
+            By.XPATH, "//input[contains(@class, 'react-autosuggest__input')]")
+        
+        #Run test card Black Lotus
+        search_box.send_keys('Black Lotus')
+        search_box.send_keys(Keys.RETURN)
+
+        try:
+            rows_pp_box = driver.find_element(
+                By.XPATH, "/html/body/div/div[3]/div/div/div/table/tfoot/tr/td/div/div/div/div[2]/div")
+            rows_pp_box.click()
+            rows_500 = driver.find_element(
+                By.XPATH, "/html/body/div[2]/div[3]/ul/li[3]")
+            rows_500.click()
+        except:
+            self.update_log_box('Could not increase rows per page, this may cause cards to be missed. Could be worth restarting')
+
+        #Open manabox collection file
+        with open(self.collection_path, 'r') as file:
+            csv_reader = csv.reader(file)
+            for row in csv_reader:
+                self.collection.append(row)
+            header = self.collection.pop(0)
+        for row in self.collection:
+            row = [str(row[0]), str(row[2]), str(row[4]), int(row[6]), str(row[8])]
+            self.cards.append(row)
+        self.update_log_box("Collection imported.")
+            
+
+        begin_time = time()
+        first_card_added = False
+        self.update_log_box("Buylist checking begun.")
+        
+        for i, card in enumerate(self.cards):
+            try:
+                window_title = driver.title
+            except:
+                self.update_log_box('Chrome window seems to have been closed. Aborting search. You can try again.')
+                break
+            
+            self.progress_update.emit(i/len(self.cards)*100)
+            try:
+                self.itemsPerSec.setText(str(np.round((i+1)/(time()-begin_time),2)) + " items per second.")
+            except:
+                pass
+            
+            try:
+                num_in_buylist = int(driver.find_element(By.XPATH, '/html/body/nav/div[1]/ul[1]/li[5]/div[1]/span').text)
+            except:
+                if first_card_added == False:
+                    num_in_buylist = 0
+                else:
+                    self.update_log_box('Unable to get the number of cards in buylist. Code will NOT automatically stop at 300 cards.')
+                
+            if num_in_buylist >= 300:
+                self.update_log_box('Buylist maximum reached!')
+                self.show_full_message.emit()
+                
+                self.mutex.lock()
+                self.wait_condition.wait(self.mutex)
+                self.mutex.unlock()
+                
+                self.update_log_box('Checking continued.')
+
+            try:
+                search_box = driver.find_element(
+                    By.XPATH, "//input[contains(@class, 'react-autosuggest__input')]")
+                #enters card details into the box
+                search_box.send_keys(card[0])
+                search_box.send_keys(Keys.RETURN)
+            except:
+                self.update_log_box(f'Issue searching for {card[0]}. Card skipped.')
+            matches = []
+            i = 0
+            end = False
+            #while loop just runs until it cant find an extra line in the table by XPATHs
+            try:
+                while end == False:
+                    i += 1
+                    try:
+                        #gets the individuial card info from table
+                        matches.append([driver.find_element(
+                        By.XPATH, f'/html/body/div/div[3]/div/div/div/div[3]/table/tbody/tr[{i}]/td[2]').text, driver.find_element(
+                        By.XPATH, f'/html/body/div/div[3]/div/div/div/div[3]/table/tbody/tr[{i}]/td[4]').text.split(' ')[0], driver.find_element(
+                        By.XPATH, f'/html/body/div/div[3]/div/div/div/div[3]/table/tbody/tr[{i}]/td[5]').text[1:]])
+                    except:
+                        end = True
+            except:
+                self.update_log_box(f'Issue when getting buylist information for {card[0]}. Card skipped.')
+            #tidies up the card info scraped from the table
+            i = 0
+            extra = False
+            for match in matches:
+                if '(' in match[0]:
+                    extra = True
+            scryfall_checked = False
+            for match in matches:
+                i += 1
+                card_name, card_set = match[0].split('\n')
+                try:
+                    card_name, card_foil = card_name.split(' · ')
+                except:
+                    card_foil = 'Normal'
+                card_quantity = int(match[1])
+                card_price = float(match[2])
+                if card_set == card[1]:
+                    if extra == True and scryfall_checked == False:
+                        try:
+                            response = requests.get(f"https://api.scryfall.com/cards/{card[4]}")
+                            if response.status_code == 200:
+                                card_data = response.json()
+                                
+                                #check for borderless
+                                if 'border_color' in card_data:
+                                    if card_data['border_color'] == 'borderless':
+                                        card[0] = card[0] + ' (Borderless)'
+                                
+                                #check for showcase
+                                if 'frame_effects' in card_data:
+                                    if "showcase" in card_data['frame_effects']:
+                                        card[0] = card[0] + ' (Showcase)'
+                                        
+                                    if "extendedart" in card_data['frame_effects']:
+                                        card[0] = card[0] + ' (Extended Art)'
+                                
+                                #check for retro frame
+                                if 'frame' in card_data:
+                                    if card_data['frame'] == '1997':
+                                        card[0] = card[0] + ' (Retro Frame)'
+                            scryfall_checked = True
+                        except:
+                            self.update_log_box(f'Issue when getting scryfall information for {card_name}. Card skipped.')
+                    
+                    if card_foil.lower() == card[2].lower():
+                        if card[0] == card_name:
+                            if card_quantity > 0:
+                                if card[3] - self.spare_quantity > 0:
+                                    if card_quantity < card[3]-self.spare_quantity:
+                                        num_to_sell = card_quantity-self.spare_quantity
+                                    else:
+                                        num_to_sell = card[3]-self.spare_quantity
+                                    try:
+                                        sell_button = driver.find_element(By.XPATH, f'/html/body/div/div[3]/div/div/div/div[3]/table/tbody/tr[{i}]/td[7]/div/div/button')
+                                        sell_button.click()
+                                        quantity_button = driver.find_element(By.XPATH, f'/html/body/div[2]/div[3]/div/div/div[{num_to_sell+1}]/div/button')
+                                        quantity_button.click()
+                                        self.update_log_box(f'{num_to_sell}x {card_name} from {card_set} added to buylist at {card_price}.')
+                                        first_card_added = True
+                                    except:
+                                        self.update_log_box(f'Issue when adding {card_name} to buylist. Card skipped.')
+            self.update_log_box(f"{card[0]} checked.")
+            sleep(0.15)
+            
+        self.update_log_box("\nBuylist adding complete.")
+        driver.close()
+            
+config = configparser.ConfigParser()
+
+class MainWindow(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        
+        uic.loadUi('mainwindow.ui', self)
+        self.resize(600, 450)
+        self.setContentsMargins(20,20,20,20)
+        
+        if os.path.exists("config.ini"):
+            config.read("config.ini")
+            self.usernameBox.setText(config["Info"]["username"])
+            self.passwordBox.setText(config["Info"]["password"]) 
+            self.collectionPath.setText(config["Info"]["path"]) 
+            self.rememberBox.setChecked(True)
+        
+        self.goButton.pressed.connect(self.go)
+        self.browseButton.pressed.connect(self.browse_file)
+            
+    def browse_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Select a File", "", "CSV Files (*.csv);;All Files (*)")
+        if file_path:  # If a file is selected
+            self.collectionPath.setText(file_path)  # Show file path in text box
+            
+    def max_reached(self):
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("Maximum Buylist amount reached!")
+        msg_box.setText("The maximum Buylist order size (300 cards) has been reached. Please submit this buylist order THEN push OK.")
+        msg_box.setIcon(QMessageBox.Icon.Warning)
+        msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
+        
+        response = msg_box.exec()
+        if response == QMessageBox.StandardButton.Ok:
+            self.worker.resume()
+        
+    def go(self):
+        username = self.usernameBox.text()
+        password = self.passwordBox.text()
+        spare_quantity = self.spareBox.text()
+        collection_path = self.collectionPath.text()
+        config["Info"] = {"username": username, "password": password, "path": collection_path}
+        if self.rememberBox.isChecked():
+            with open("config.ini", "w") as configfile:
+                config.write(configfile)
+        self.worker = WorkerThread()
+        self.worker.details(username, password, collection_path, spare_quantity, self.logBox, self.itemsPerSec)
+        self.worker.progress_update.connect(self.progressBar.setValue)
+        self.worker.show_full_message.connect(self.max_reached)
+        self.worker.start()
+        
+        
+        
+if not QApplication.instance():
+    app = QApplication(sys.argv)
+else:
+    app = QApplication.instance()
+    
+if __name__ == "__main__":    
+    app.setStyle('windowsvista')
+    main = MainWindow()
+    main.show()
+    app.exec()
---
+++ readme.txt
@@ -0,0 +1,20 @@
+hey babes
+
+when you run the mtgmate checker, it should open a little window to type your detials into, and select the collection.
+you can select if you want it to subtract a number from cards to sell, for example if you want to keep 1 of each card, and attempt to sell the rest.
+when you push go, it should open a chrome window, go to mtgmate and log you in. it should then search for a test card (Black Lotus).
+if it gets this far then it should (*should*) be safe to leave it running - you can use your pc and it will still work in the background.
+
+there is a progress bar to tell you how far through your collection it is, and info telling you how fast it is going.
+once it is done, you can open mtgmate in your normal browser and all the cards you supposedly have will be in the buylist cart, they should all be there unless you delete your cookies or use incognito mode or whatever.
+remove the ones you do not want to sell and then complete the sale.
+
+
+
+NOTES:
+requires modules 'selenium' and 'PyQt6' to be pip installed prior to running.
+seems to only work if you use chrome! (need chrome installed at least). if you get an error saying that it is unable to find a driver for chrome, open this link: https://storage.googleapis.com/chrome-for-testing-public/134.0.6998.35/win64/chrome-win64.zip and extract it to the folder where the code is. run the setup.exe and hopefully that fixes it
+technically according to mtgmates back-end notes, bot trawling is not allowed, but they havent seemed to block it at all - so use it at your own risk i guess?
+sometimes when logging in, the reCaptcha catches the bot. in my experience, just try it again and it more often than not, doesnt catch the bot.
+try not to click inside the browser window as it is running, it might cause it to crash. it *shouldnt*, as ive set up error catchers, but just to be safe.
+the code should automatically stop if you reach the maximum buylist order of 300 cards. the program will pause and wait for you to complete the order, then you can push OK and it will begin a new one.
